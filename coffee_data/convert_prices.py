b782adc7d0f875300f5cc73f8fc54e074389af2c
SymeonWhite <symeonwhite16@gmail.com>
2020-01-10 15:44:59 -0600

    created csv for exports of coffee from colombia

--- coffee_data/convert_prices.py
+++ coffee_data/convert_prices.py
@@ -6,16 +6,19 @@ df = pd.read_csv("excelso_prices.csv")
 
 
 def adjust_for_inflation(df):
-    final = []
+    adj = []
+    rounded_non_adj = []
     for row in df.itertuples():
         year = int(row.date[:4])
         month = int(row.date[5:7])
-        price = round(int(row.excelso_price_cents_per_pound))/10
+        price = round(int(row.excelso_price_cents_per_pound),2)
         out = round(cpi.inflate(price, date(year,month,1)))
-        final.append(out)
-    return final
+        adj.append(out)
+        rounded_non_adj.append(price)
+    return adj, rounded_non_adj
 
-out = adjust_for_inflation(df)
+adj,non_adj = adjust_for_inflation(df)
 
-df["price_adjusted_inflation"] = out
+df["price_adjusted_inflation"] = adj
+df["excelso_price_cents_per_pound"] = non_adj 
 df.to_csv("price_adjusted_inflation.csv", index=False)
---
+++ coffee_data/importers.py
@@ -0,0 +1,2 @@
+import pandas as pd 
+pd.read_csv("country_export.tsv", sep="\t",encoding="utf-8")
